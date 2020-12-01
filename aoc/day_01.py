3824360aaccdc65ac4aac11e0bb664d957b59689
Marcel Neumann <marcel.neumann@rewe-digital.com>
2020-12-01 22:12:00 +0100

    Add part_02 of day_01

--- aoc/day_01.py
+++ aoc/day_01.py
@@ -1,5 +1,8 @@
 from typing import List
 
+import math
+from itertools import combinations
+
 from aoc.utils import read_list
 
 
@@ -15,11 +18,18 @@ def part_01(expense_report: List[int]):
         seen.add(x)
 
 
+def part_02(expense_report: List[int]):
+    for comb in combinations(expense_report, 3):
+        if sum(comb) == 2020:
+            return math.prod(comb)
+
+
 def main():
     with open("../input/input_01.txt") as f:
         puzzle = read_list(f)
 
     print(f"Part 01: {part_01(puzzle)}")
+    print(f"Part 02: {part_02(puzzle)}")
 
 
 if __name__ == "__main__":
--- tests/test_day_01.py
+++ tests/test_day_01.py
@@ -1,5 +1,9 @@
-from aoc.day_01 import part_01
+from aoc.day_01 import part_01, part_02
 
 
 def test_part_01():
     assert part_01([1721, 979, 366, 299, 675, 1456]) == 514579
+
+
+def test_part_02():
+    assert part_02([1721, 979, 366, 299, 675, 1456]) == 241861950
